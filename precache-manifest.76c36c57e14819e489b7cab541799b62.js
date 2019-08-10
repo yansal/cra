620086d597f432f0bcedc0fa70ae366453d44495
Yann Salaün <yann@ulule.com>
2019-08-10 14:31:15 +0200

    Updates

---
+++ precache-manifest.76c36c57e14819e489b7cab541799b62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20341e8baaadf797cd634895d936bbd7",
+    "url": "/cra/index.html"
+  },
+  {
+    "revision": "19e5125b9bd956489f5a",
+    "url": "/cra/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ede490a44a0ad3754691",
+    "url": "/cra/static/js/2.1c4a84ed.chunk.js"
+  },
+  {
+    "revision": "19e5125b9bd956489f5a",
+    "url": "/cra/static/js/main.7e12ab97.chunk.js"
+  },
+  {
+    "revision": "e92a4074ee2e02d0fc91",
+    "url": "/cra/static/js/runtime~main.c4069811.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cra/static/media/logo.5d5d9eef.svg"
+  }
+]);
